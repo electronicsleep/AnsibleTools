e18ee01931632ed830cd3a322fc59f06d649078e
Chris <electronicsleep@gmail.com>
2017-08-29 18:12:18 -0700

    Improved update logging

--- roles/update-os/files/update.sh
+++ roles/update-os/files/update.sh
@@ -1,8 +1,16 @@
+# Author: https://github.com/electronicsleep
+# Date: 07/20/2017
+# Purpose: Ansible - Check Apt updates and log details to a file
+# Released under the BSD license
+
 #!/bin/bash
 DATE=$(date +%Y%m%d-%H%M)
 echo "DATE: $DATE"
-sudo apt-get update ; sudo apt-get upgrade --dry-run > /tmp/update_$DATE.log
-if grep "0 upgraded, 0 newly installed, 0 to remove" /tmp/update_$DATE.log; then
+# Run Updates and save to file
+sudo apt-get update ; sudo apt-get upgrade --dry-run > /tmp/apt-update.log
+# Create update log by timestamp
+cat /tmp/apt-update.log > /tmp/apt-update-$DATE.log
+if grep "0 upgraded, 0 newly installed, 0 to remove" /tmp/apt-update.log; then
 	echo "*** NO UPDATES ***"
 else
 	echo "*** UPDATES PENDING ***"
