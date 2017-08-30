67c6136a80e76eb9ca84971f5d90c5cfbddb7d8d
Chris <electronicsleep@gmail.com>
2017-08-29 18:32:12 -0700

    Updates to logging

--- roles/update-os/files/update.sh
+++ roles/update-os/files/update.sh
@@ -5,13 +5,13 @@
 
 #!/bin/bash
 DATE=$(date +%Y%m%d-%H%M)
-echo "DATE: $DATE"
+echo "DATE: $DATE" | tee /tmp/apt-update.log
 # Run Updates and save to file
-sudo apt-get update ; sudo apt-get upgrade --dry-run > /tmp/apt-update.log
+sudo apt-get update ; sudo apt-get upgrade --dry-run | tee -a /tmp/apt-update.log
+if grep "0 upgraded, 0 newly installed, 0 to remove" /tmp/apt-update.log; then
+	echo "### NO UPDATES ###" | tee -a  /tmp/apt-update.log
+else
+	echo "### UPDATES PENDING ###" | tee -a /tmp/apt-update.log
+fi
 # Create update log by timestamp
 cat /tmp/apt-update.log > /tmp/apt-update-$DATE.log
-if grep "0 upgraded, 0 newly installed, 0 to remove" /tmp/apt-update.log; then
-	echo "*** NO UPDATES ***"
-else
-	echo "*** UPDATES PENDING ***"
-fi
