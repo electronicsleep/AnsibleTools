50f1bfc8fefcf58e8fb9d0bec1aa0bb7687406e5
Chris <electronicsleep@gmail.com>
2018-02-22 23:46:47 -0800

    Ansible Python Example

--- ansiblePython.py
+++ ansiblePython.py
@@ -7,15 +7,16 @@
 
 import subprocess
 import argparse
-
 import datetime
+
 date = datetime.datetime.now().strftime("%Y%m%d-%H%M")
-print("date: " + date)
+print("report date: " + date)
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("-p", "--playbook", help="playbook to run", required=False)
+    parser.add_argument("-u", "--user", help="user for host", required=False)
     args = parser.parse_args()
 
     if args.playbook is None:
@@ -23,12 +24,22 @@ def main():
     else:
         playbook = args.playbook
 
+    if args.user is None:
+        user = "ubuntu"
+    else:
+        user = args.user
+
     print("playbook: ", playbook)
-    user = "ubuntu"
+    print("user: ", user)
 
     hosts_file = open("/etc/ansible/hosts", "r")
     report = open("report-" + date + ".log", 'w')
 
+    disk_space_check = []
+    for size in range(80, 100):
+        disk_space_check.append(str(size) + "%")
+
+    found_host_issue = []
     host_num = 0
     for host_line in hosts_file:
         if "[" in host_line:
@@ -49,7 +60,17 @@ def main():
             report.write("Host Details: " + host_line)
             for line in output.decode("utf-8").split("\n"):
                 print(line.strip())
+                for disk_space in disk_space_check:
+                    if disk_space in line:
+                        print("Found: ", disk_space)
+                        found_host_issue.append(host_line)
+
                 report.write(line + "\n")
+
+    if len(found_host_issue) == 0:
+        print("No issues found")
+    for host in found_host_issue:
+        print("Issue: " + host)
 
     report.close()
 
