2fc836f4d2aa82889ec4df62796b0f5f2f476699
Chris <electronicsleep@gmail.com>
2018-02-22 23:34:07 -0800

    Ansible Python Example

--- ansiblePython.py
+++ ansiblePython.py
@@ -6,27 +6,53 @@
 # Released under the BSD license
 
 import subprocess
+import argparse
 
-user = "ubuntu"
+import datetime
+date = datetime.datetime.now().strftime("%Y%m%d-%H%M")
+print("date: " + date)
 
-hosts_file = open("/etc/ansible/hosts", "r")
 
-host_num = 0
-for host_line in hosts_file:
-    if "[" in host_line:
-        group = host_line
-        print("Group: ", host_line.strip())
-    elif host_line != "":
-        host_num += 1
-        print("Host Details:", host_line.strip())
-        print("Host Num:", host_num)
-        host = host_line.split(" ")[0]
-        print("Address:", host)
-        host_one = open("ansible_host", 'w')
-        host_one.write("[default]\n")
-        host_one.write(host + "\n")
-        host_one.close()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--playbook", help="playbook to run", required=False)
+    args = parser.parse_args()
 
-        output = subprocess.check_output(['ansible-playbook', 'check-disk.yml', '-i', "ansible_host", "-u", user])
-        for line in output.decode("utf-8").split("\n"):
-            print(line.strip())
+    if args.playbook is None:
+        playbook = "check-disk.yml"
+    else:
+        playbook = args.playbook
+
+    print("playbook: ", playbook)
+    user = "ubuntu"
+
+    hosts_file = open("/etc/ansible/hosts", "r")
+    report = open("report-" + date + ".log", 'w')
+
+    host_num = 0
+    for host_line in hosts_file:
+        if "[" in host_line:
+            group = host_line
+            print("Group: ", group)
+        elif host_line != "":
+            host_num += 1
+            print("Host Details:", host_line.strip())
+            print("Host Num:", host_num)
+            host = host_line.split(" ")[0]
+            print("Address:", host)
+            host_one = open("ansible_host", 'w')
+            host_one.write("[default]\n")
+            host_one.write(host + "\n")
+            host_one.close()
+
+            output = subprocess.check_output(['ansible-playbook', playbook, '-i', "ansible_host", "-u", user])
+            report.write("Host Details: " + host_line)
+            for line in output.decode("utf-8").split("\n"):
+                print(line.strip())
+                report.write(line + "\n")
+
+    report.close()
+
+
+if __name__ == '__main__':
+    main()
